5e90f2eadcd1a9f8aade31f379235376f0c40c63
osakoyohei <sako4.nsr@gmail.com>
2021-07-06 01:24:50 +0900

    make it better

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,8 +2,8 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=960">
-    
+    <meta name="viewport" content="width=600">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
