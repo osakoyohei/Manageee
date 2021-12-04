d260df6eb029812713c115e19fa256b020959043
osakoyohei <sako4.nsr@gmail.com>
2021-12-04 20:10:38 +0900

    ドロップダウンメニュー追加

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,10 +9,20 @@
                     <a href="{{ route('todo.create') }}" class="btn btn-link" style="color:white;">ToDo登録 <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button class="btn btn-link" style="color:silver;">ログアウト</button>
-            </form>
+            <div class="btn-group">
+                <button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ユーザー情報
+                </button>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <button class="dropdown-item" type="button">プロフィール</button>
+                    <button class="dropdown-item" type="button">アカウント情報</button>
+                    <button class="dropdown-item" type="button">ToDo完了履歴</button>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="dropdown-item">ログアウト</button>
+                    </form>
+                </div>
+            </div>
         @else
             <a href="{{ route('login.show') }}" class="btn btn-link" style="color:white;">ログイン</a>
             <a href="{{ route('register.show') }}" class="btn btn-link" style="color:white;">新規登録</a>
