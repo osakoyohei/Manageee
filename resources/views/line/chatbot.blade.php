e8c33a48ba1928f972103eaf787ced6792319adb
osakoyohei <sako4.nsr@gmail.com>
2022-01-14 19:28:42 +0900

    ラインチャットボット機能編集

--- resources/views/line/chatbot.blade.php
+++ resources/views/line/chatbot.blade.php
@@ -12,10 +12,10 @@
 
 <!-- LINEチャットボット -->
 <div class="chatbot-explanation">
-    <h2>名言チャットボット</h2><br>
-    <h4>名言チャットボットを友達追加して、<br>毎朝届く偉人の名言でモチベーションを上げよう！</h4>
+    <h2>☆名言チャットボット☆</h2><br>
+    <h4 >名言チャットボットを友達追加して、<br>毎朝届く偉人の名言でモチベーションを上げよう！</h4>
     <a href="https://lin.ee/1O8PgNv" target="_blank" rel="noopener noreferrer">
-        <img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"  class="my-3" alt="友だち追加" height="36" border="0">
+        <img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"  class="my-4" alt="友だち追加" height="36" border="0">
     </a>
 </div>
 
