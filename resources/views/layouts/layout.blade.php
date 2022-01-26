4dfe3f60c9377aa15c4198c85f8f67a7b3879679
osakoyohei <sako4.nsr@gmail.com>
2022-01-27 01:31:29 +0900

    viewport編集

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,8 +2,8 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=800">
-
+    <meta name=”viewport” content=”width=device-width,initial-scale=1.0,minimum-scale=1.0″>
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
